93aaba77933d44303a889cbe38e7596ae01fbbe1
Ruchita019 <ruchitambox19@gmail.com>
2024-06-14 12:07:08 +0530

    commit

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, request
+import os
+import requests
+from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
+import google.generativeai as genai
+import markdown2
 
-# from backend import routes
+
 # from config import config
-
-
-app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
 # app.config.from_object(Config)
 
-# Initialize routes
-# routes.init_routes(app)
+load_dotenv()
+app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
+
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+model = genai.GenerativeModel('gemini-1.5-flash')
+
 
 @app.route('/')
 def index():
@@ -27,7 +32,19 @@ def submit():
             activities = request.form['activities']
 
             # Process the data (e.g., store in a database, perform some logic, etc.)
-            return f"Place: {place}, Start-Date: {startdate}, End-Date: {enddate}, No. of people: {people}, Budget: {budget}, Activities: {activities}"
+        #     return f"Place: {place}, Start-Date: {startdate}, End-Date: {enddate}, No. of people: {people}, Budget: {budget}, Activities: {activities}"
+        
+            prompt = f'''I am planning a trip to ${place}. My budget is ${budget} rupees for ${people} people. 
+                        The trip will start on ${startdate} and end on ${enddate}.Here is a brief description of activities 
+                        they would like to do on trip: ${activities}. Please provide a detailed itinerary and recommendations 
+                        as per my interest ans also recommend the hotels . '''
+
+            response = model.generate_content(prompt)
+            markdown_content = response.text
+            itinerary_data = markdown2.markdown(markdown_content)
+
+            return render_template('index_itinerary.html', itinerary_data=itinerary_data)
+        return render_template('index_itinerary.html')
 
 
 
